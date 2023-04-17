bf99fe7ae5345b047a48a6c836397135e01bb5ee
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-04-17 08:14:25 -0300

    removes module federation plugin

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
-const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
-
 const nextConfig = {
   webpack: (config, options) => {
     const { isServer } = options;
     config.experiments = { topLevelAwait: true, layers: true };
-
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "web",
-        remotes: {
-          cart: `cart@http://localhost:3001/_next/static/chunks/remoteEntry.js`,
-        },
-        filename: "static/chunks/remoteEntry.js",
-        shared: {},
-      })
-    );
 
     return config;
   },
