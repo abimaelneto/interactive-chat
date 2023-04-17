fe7f028c2582e4f4fedd49408d90cbb5cb9891c3
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-04-17 08:55:29 -0300

    adds reference on firebase

--- src/libs/firebase/index.js
+++ src/libs/firebase/index.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref } from "firebase/database";
 import config from "./config.js";
 
 const app = getApps().length < 1 ? initializeApp(config()) : getApp();
 
 export const database = getDatabase(app);
+export const rootRef = ref(database);
+export const dbChats = ref(database, "/chats");
